6bf3dbf4e566401110699050b04cf02831fd9b67
rivera-bl <rivera.blo@protonmail.com>
2021-07-18 20:22:39 -0400

    Function to open_telescope() when opening a dir

--- lua/telescope_lua/init.lua
+++ lua/telescope_lua/init.lua
@@ -57,4 +57,19 @@ M.search_git = function()
   })
 end
 
+_G.open_telescope = function()
+    local first_arg = vim.v.argv[2]
+    if first_arg and vim.fn.isdirectory(first_arg) == 1 then
+        vim.g.loaded_netrw = true
+        require("telescope.builtin").find_files({search_dirs = {first_arg}})
+    end
+end
+
+vim.api.nvim_exec([[
+augroup TelescopeOnEnter
+    autocmd!
+    autocmd VimEnter * lua open_telescope()
+augroup END
+]], false)
+
 return M
