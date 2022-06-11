289ca194b2efbea4e812307f7c6743386e1e0f0e
rivera-bl <rivera.blo@protonmail.com>
2022-06-10 22:11:12 -0400

    Fix packer.nvim autodelete every PackerSync

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,13 +1,11 @@
 -- Auto install packer.nvim if not exists
 local fn = vim.fn
 local execute = vim.api.nvim_command
-local install_path = fn.stdpath('data')..'/site/pack/packer/opt/packer.nvim'
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
 
 if fn.empty(fn.glob(install_path)) > 0 then
   execute('!git clone https://github.com/wbthomason/packer.nvim '..install_path)
 end
-
-vim.cmd('packadd packer.nvim')
 
 -- packer as a floating window
 require("packer").init {
