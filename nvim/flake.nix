36dfee625f0ded528d60ea7f9fbc5ced90a05d4d
rivera-bl <rivera.pablo1090@gmail.com>
2022-07-16 19:20:45 -0400

    nvim: basic flake test

--- nvim/flake.nix
+++ nvim/flake.nix
@@ -9,6 +9,31 @@
     self,
     nixpkgs,
     flake-utils,
-  }: {
-  };
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+    in {
+      packages = {
+        defaultPackage = with pkgs;
+          neovim.override {
+            vimAlias = true;
+            viAlias = true;
+            configure = {
+              packages.my-plugins = with pkgs.vimPlugins; {
+                start = [
+                  nvim-telescope
+                  nvim-lspconfig
+                ];
+                opt = [];
+              };
+
+              customRC = ''
+                lua << EOF
+                  ${builtins.readFile} ./lua/settings.lua
+                EOF
+              '';
+            };
+          };
+      };
+    });
 }
