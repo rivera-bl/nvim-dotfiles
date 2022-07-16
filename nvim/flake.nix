4323a789b4af015fb09fd57f0e7696139db18ab0
rivera-bl <rivera.pablo1090@gmail.com>
2022-07-16 16:56:55 -0400

    nvim: init flake

---
+++ nvim/flake.nix
@@ -0,0 +1,14 @@
+{
+  description = "Neovim Flake";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }: {
+  };
+}
